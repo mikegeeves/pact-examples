eacf9da0da7b4e55497080f555399c5be8590e1a
Yousaf Nabi <you@saf.dev>
2022-08-02 22:46:47 +0100

    feat: add v3 js provider and consumer (jest-pact)

---
+++ consumer-features/example-hello-world/v3/example-hello-world-js-jest-pact/tests/consumer/consumer.test.js
@@ -0,0 +1,47 @@
+// Pact annotated code block - Setting up the Consumer
+const { pactWith } = require("jest-pact/v3");
+const { BearApiClient } = require("../../src/consumer");
+
+// Pact annotated code block - Setting up the Consumer
+pactWith(
+  {
+    consumer: "BearServiceClient",
+    provider: "BearService",
+    dir: "./output/pacts",
+  },
+  // End Pact annotated code block
+  (interaction) => {
+    interaction("test bear endpoint", ({ provider, execute }) => {
+      //  Pact annotated code block - Defining the pact, and calling the consumer
+
+      const expectedResponse = {
+        name: "Polar",
+        colour: "White",
+      };
+
+      beforeEach(() =>
+        provider
+          .given("Some bears exist")
+          .uponReceiving("a request for the Polar bear species")
+          .withRequest({
+            method: "GET",
+            path: "/species/Polar",
+          })
+          .willRespondWith({
+            status: 200,
+            body: {
+              status: 200,
+              body: expectedResponse,
+            },
+          })
+      );
+
+      execute("returns a bear", (mockserver) =>
+        new BearApiClient(mockserver.url).getSpecies("Polar").then((resp) => {
+          expect(resp).toEqual(new Bear({ expectedResponse }));
+        })
+      );
+      //  End Pact annotated code block
+    });
+  }
+);
